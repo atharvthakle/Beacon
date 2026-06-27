f7a3c3a448a721f140a63c3a7f7fcbceeb55bcc4
Atharv Thakle <136578804+atharvthakle@users.noreply.github.com>
2026-06-27 15:44:42 +0530

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -17,6 +17,7 @@ app.use(cors({
     'http://localhost:5173',
     'http://localhost:3000',
     'http://localhost:4173'
+    'https://beacon-beta-dusky.vercel.app',
   ],
   methods: ['GET', 'POST'],
   allowedHeaders: ['Content-Type'],
